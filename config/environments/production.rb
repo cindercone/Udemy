8c79c1690d7b991560febf9229221c9953459008
myricktc <myricktc@gmail.com>
2016-10-16 21:09:36 -0700

    another try at fixing s3 uploads

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
   },
-  :s3_host_name => "s3-eu-west-1.amazonaws.com", # Added entry
+  :s3_host_name => "s3-us-west-2.amazonaws.com", # Added entry
   :url => ":s3_host_name"                        # Added entry
 
 }
